3aad485de3b5e10d3692068670741fb0e02c066f
Lucas Duarte <lucas.duarte@rivendel.com.br>
2018-03-02 01:57:09 -0300

    Finalizado os exercicios da aula 02 de prog comp utilizando testes do JUnity

---
+++ database_paises.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS Pais;
+
+USE Pais;
+
+CREATE TABLE IF NOT EXISTS paises (
+    id int auto_increment primary key not null,
+    nome varchar(100) not null,
+    populacao bigint not null,
+    area decimal(15,2) not null
+    );
+    
+
+INSERT INTO paises(nome,populacao,area) VALUES('Brasil',225000000,44587.00);
+INSERT INTO paises(nome,populacao,area) VALUES('China',45200000000,24577.00);
+INSERT INTO paises(nome,populacao,area) VALUES('Canada',2850000,49887.00);
+INSERT INTO paises(nome,populacao,area) VALUES('Argentina',40000000,57000.00);
+INSERT INTO paises(nome,populacao,area) VALUES('Bolivia',48700000,33478.00);
+
+SELECT * FROM paises;
